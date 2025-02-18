3de02bbdfaa90ed4e5265e58180b5d1ea65867e3
Jackson Porciúncula <jbrunops@outlook.com>
2025-02-18 16:46:41 -0300

    tickets updateStatus

---
+++ src/controllers/tickets/updateStatus.js
@@ -0,0 +1,11 @@
+export function updateStatus({ request, response, database }) {
+    const { id } = request.params;
+    const { solution } = request.body;
+    console.log(solution);
+    database.update("tickets", id, {
+        status: "closed",
+        updated_at: new Date(),
+        solution,
+    });
+    return response.end();
+}
